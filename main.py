9b3c801ca3aaaede3bf17b4c2ade315f8166de83
Shrestharustam-009 <rustamshrestha0123@gmail.com>
2025-07-07 11:35:08 +0545

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from abc import ABC, abstractmethod
 
-
+# Abstract Class
 class StudentManagementSystem(ABC):
     @abstractmethod
     def admin_log():
